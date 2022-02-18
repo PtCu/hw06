3e9c88191dff3fbb25f8960d2ea6ec41a5d647c9
PtCu <1365699009@qq.com>
2022-02-18 15:45:25 +0800

    first

--- main.cpp
+++ main.cpp
@@ -5,60 +5,101 @@
 #include <numeric>
 #include <algorithm>
 #include "ticktock.h"
+#include <tbb/tbb.h>
 
 // TODO: 并行化所有这些 for 循环
 
 template <class T, class Func>
-std::vector<T> fill(std::vector<T> &arr, Func const &func) {
+std::vector<T> fill(std::vector<T> &arr, Func const &func)
+{
     TICK(fill);
-    for (size_t i = 0; i < arr.size(); i++) {
-        arr[i] = func(i);
-    }
+    tbb::parallel_for(tbb::blocked_range<size_t>(0, arr.size()),
+                      [&](tbb::blocked_range<size_t> r)
+                      {
+                          for (size_t i = r.begin(); i < r.end(); ++i)
+                          {
+                              arr[i] = func(i);
+                          }
+                      });
     TOCK(fill);
     return arr;
 }
 
 template <class T>
-void saxpy(T a, std::vector<T> &x, std::vector<T> const &y) {
+void saxpy(T a, std::vector<T> &x, std::vector<T> const &y)
+{
     TICK(saxpy);
-    for (size_t i = 0; i < x.size(); i++) {
-       x[i] = a * x[i] + y[i];
-    }
+    tbb::parallel_for(tbb::blocked_range<size_t>(0, x.size()),
+                      [&](tbb::blocked_range<size_t> r)
+                      {
+                          for (size_t i = r.begin(); i < r.end(); ++i)
+                          {
+                              x[i] = a * x[i] + y[i];
+                          }
+                      });
     TOCK(saxpy);
 }
 
 template <class T>
-T sqrtdot(std::vector<T> const &x, std::vector<T> const &y) {
+T sqrtdot(std::vector<T> const &x, std::vector<T> const &y)
+{
     TICK(sqrtdot);
-    T ret = 0;
-    for (size_t i = 0; i < std::min(x.size(), y.size()); i++) {
-        ret += x[i] * y[i];
-    }
+    T ret = tbb::parallel_reduce(
+        tbb::blocked_range<size_t>(0, x.size()), 0,
+        [&](tbb::blocked_range<size_t> r, T local_res)
+        {
+            for (size_t i = r.begin(); i < r.end(); ++i)
+            {
+                local_res += x[i] * y[i];
+            }
+            return local_res;
+        },
+        [](T x, T y)
+        {
+            return x + y;
+        });
     ret = std::sqrt(ret);
     TOCK(sqrtdot);
     return ret;
 }
 
 template <class T>
-T minvalue(std::vector<T> const &x) {
+T minvalue(std::vector<T> const &x)
+{
     TICK(minvalue);
-    T ret = x[0];
-    for (size_t i = 1; i < x.size(); i++) {
-        if (x[i] < ret)
-            ret = x[i];
-    }
-    TOCK(minvalue);
+
+    T ret = tbb::parallel_reduce(
+        tbb::blocked_range<size_t>(0, x.size()), 0,
+        [&](tbb::blocked_range<size_t> r, T local_res)
+        {
+            for (size_t i = r.begin(); i < r.end(); ++i)
+            {
+                if (x[i] < local_res)
+                    local_res = x[i];
+            }
+            return local_res;
+        },
+        [](T x, T y)
+        {
+            return x < y ? x : y;
+        });
+
     return ret;
 }
 
 template <class T>
-std::vector<T> magicfilter(std::vector<T> const &x, std::vector<T> const &y) {
+std::vector<T> magicfilter(std::vector<T> const &x, std::vector<T> const &y)
+{
     TICK(magicfilter);
     std::vector<T> res;
-    for (size_t i = 0; i < std::min(x.size(), y.size()); i++) {
-        if (x[i] > y[i]) {
+    for (size_t i = 0; i < min(x.size(), y.size()); i++)
+    {
+        if (x[i] > y[i])
+        {
             res.push_back(x[i]);
-        } else if (y[i] > x[i] && y[i] > 0.5f) {
+        }
+        else if (y[i] > x[i] && y[i] > 0.5f)
+        {
             res.push_back(y[i]);
             res.push_back(x[i] * y[i]);
         }
@@ -68,24 +109,40 @@ std::vector<T> magicfilter(std::vector<T> const &x, std::vector<T> const &y) {
 }
 
 template <class T>
-T scanner(std::vector<T> &x) {
+T scanner(std::vector<T> &x)
+{
     TICK(scanner);
-    T ret = 0;
-    for (size_t i = 0; i < x.size(); i++) {
-        ret += x[i];
-        x[i] = ret;
-    }
+    auto ret = tbb::parallel_scan(
+        tbb::blocked_range<std::size_t>(0, x.size()), T{},
+        [&](auto r, auto local_res, auto is_final)
+        {
+            for (auto i = r.begin(); i != r.end(); ++i)
+            {
+                local_res += x[i];
+                if (is_final)
+                {
+                    x[i] = local_res;
+                }
+            }
+            return local_res;
+        },
+        [](auto x, auto y)
+        { return x + y; });
+
     TOCK(scanner);
     return ret;
 }
 
-int main() {
-    size_t n = 1<<26;
+int main()
+{
+    size_t n = 1 << 26;
     std::vector<float> x(n);
     std::vector<float> y(n);
 
-    fill(x, [&] (size_t i) { return std::sin(i); });
-    fill(y, [&] (size_t i) { return std::cos(i); });
+    fill(x, [&](size_t i)
+         { return std::sin(i); });
+    fill(y, [&](size_t i)
+         { return std::cos(i); });
 
     saxpy(0.5f, x, y);
 
